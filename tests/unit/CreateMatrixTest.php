ed0c1dad14a50261879b840aa2ca129db8781d29
Petr Grishin <petr.grishin@grishini.ru>
2015-03-04 01:16:06 +0300

    Исправление тестов под 5.3

--- tests/unit/CreateMatrixTest.php
+++ tests/unit/CreateMatrixTest.php
@@ -8,32 +8,32 @@ use \PetrGrishin\CreateMatrix\CreateMatrix;
 class CreateMatrixTest extends \PHPUnit_Framework_TestCase {
 
     public function testSquareMatrix() {
-        $params = [
-            'a' => [1, 2, null],
-            'b' => [10, 20, null],
-            'c' => [100, 200, null],
-        ];
+        $params = array(
+            'a' => array(1, 2, null),
+            'b' => array(10, 20, null),
+            'c' => array(100, 200, null),
+        );
         $matrix = CreateMatrix::byVariants($params)->getArray();
         $this->assertCount(pow(3, 3), $matrix);
     }
 
     public function testNotSquareMatrix() {
-        $params = [
-            'a' => [1, 2, null],
-            'b' => [10, 20, null],
-            'c' => [100, 200, 300, null],
-        ];
+        $params = array(
+            'a' => array(1, 2, null),
+            'b' => array(10, 20, null),
+            'c' => array(100, 200, 300, null),
+        );
         $matrix = CreateMatrix::byVariants($params)->getArray();
         $this->assertCount(pow(3, 2) * pow(4, 1), $matrix);
     }
 
     public function testNotSquare4xMatrix() {
-        $params = [
-            'a' => [1, 2, null],
-            'b' => [10, 20, null],
-            'c' => [100, 200, 300, null],
-            'd' => [1000, 2000, 3000, 4000],
-        ];
+        $params = array(
+            'a' => array(1, 2, null),
+            'b' => array(10, 20, null),
+            'c' => array(100, 200, 300, null),
+            'd' => array(1000, 2000, 3000, 4000),
+        );
         $matrix = CreateMatrix::byVariants($params)->getArray();
         $this->assertCount(pow(3, 2) * pow(4, 2), $matrix);
     }
